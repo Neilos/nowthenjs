3b80a9a03da152418d6dd4aec6fd4b918e50f39c
Neilos <nosniktatkinson@hotmail.co.uk>
2014-10-31 11:11:40 +0000

    Arguments can be passed as an array with ‘using:’

--- src/now_then.js
+++ src/now_then.js
@@ -2,8 +2,9 @@ var now = {
   get: function (thisObject, functionConfig) {
     var functionName =  functionConfig['to']
     var definingObject =  functionConfig['acting_as'] || thisObject
+    var arrayArgs = functionConfig['using'] || []
     var functionArgs = Array.prototype.slice.call(arguments, 2)
-    return definingObject[functionName].apply(thisObject, functionArgs)
+    return definingObject[functionName].apply(thisObject, arrayArgs.concat(functionArgs))
   }
 }
 
@@ -11,7 +12,9 @@ var then = {
   get: function (thisObject, functionConfig) {
     var functionName =  functionConfig['to']
     var definingObject =  functionConfig['acting_as'] || thisObject
-    var boundArgs = Array.prototype.slice.call(arguments, 2)
+    var arrayArgs = functionConfig['using'] || []
+    var functionArgs = Array.prototype.slice.call(arguments, 2)
+    var boundArgs = arrayArgs.concat(functionArgs)
     return function () {
       var executingArgs = Array.prototype.slice.call(arguments)
       var fullArgs = boundArgs.concat(executingArgs)
